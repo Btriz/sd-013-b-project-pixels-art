7cfc4054f8551d86ffcb9a495ed1a1bc098c10cb
Btriz <00btrz@gmail.com>
2021-06-04 21:51:10 -0300

    6 ao 8

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CRIAR O QUADRO
 const board = document.getElementById('pixel-board');
 
 function createPixelBoard(linesNumber) {
@@ -13,3 +14,32 @@ function createPixelBoard(linesNumber) {
   }
 }
 document.onload = createPixelBoard(5);
+
+// SELECIONAR CORES
+function selectColor(click) {
+  if(click.target.classList.contains('selected')) {
+  } else {
+    const deselect = document.querySelector('.selected');
+    deselect.classList.remove('selected');
+    click.target.classList.add('selected')
+  }
+}
+const colors = document.getElementsByClassName('color');
+
+for (let index = 0; index < colors.length; index += 1) {
+  colors[index].addEventListener('click', selectColor);
+}
+
+//COLORIR O QUADRO
+function paint(click) {
+  const selected = document.querySelector(".selected");
+  const color = window.getComputedStyle(selected, null).getPropertyValue('background-color');
+  click.target.style.backgroundColor = color;
+  
+}
+
+const px = document.getElementsByClassName('pixel');
+
+for (let index = 0; index < px.length; index += 1) {
+  px[index].addEventListener('click', paint);
+}
